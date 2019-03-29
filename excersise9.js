35cfcfb05e95bd162d8754ca5e1943458c3245f5
Restu Ardhi Ansyah S <48643710+RestuArdhi@users.noreply.github.com>
2019-03-29 15:44:07 +0700

    Add files via upload

---
+++ excersise9.js
@@ -0,0 +1,51 @@
+// TUGAS 1
+
+function shoutOut(){
+  
+    return 'Halo Function!'
+  }
+  
+  console.log(shoutOut()) 
+  
+  
+  
+  
+  // TUGAS 2
+  
+  function calculateMultiply(num1, num2){
+    
+   return num1 * num2  
+    
+  }
+  
+  
+  var num1 = 5;
+  var num2 = 6;
+  
+  var hasilPerkalian = calculateMultiply(num1,num2);
+  console.log(hasilPerkalian);
+  
+  
+  
+  
+  
+  
+  // TUGAS 3
+  
+  
+  function processSentence(nama, age, address, hobby){
+    
+    return 'Nama saya ' + nama +', umur saya '+ age +' tahun, alamat saya di ' +address +', dan saya punya hobby yaitu '+ hobby
+    
+  }
+  
+  var nama = 'Restu';
+  var age = 23;
+  var address = 'Jl. Pluto dalam';
+  var hobby = 'Bermain Tennis.'
+  
+  
+  var processSentence1 = processSentence(nama, age, address, hobby)
+  
+  console.log (processSentence1);
+  
